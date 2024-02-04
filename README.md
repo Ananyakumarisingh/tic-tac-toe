1413f02a76a610dacc787cdb7f4790fa15a1ed60
Ananyakumarisingh <ananyakumarisingh77@gmail.com>
2024-02-04 10:16:30 +0530

    Tic-Tac-Toe

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Tic-Tac-Toe
+
+<img style="margin:auto" src="./src/assets/images/tic-tac-toe.png" width="50%">
+
+### Try the game here:
+
+
+The following component diagram is used to help us think in components.
+<img src='./src/assets/images/diagram.png' width="70%">
---
+++ src/components/Reset.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import GameState from './GameState';
+
+const Reset = ({ gameState, onReset }) => {
+  if (gameState === GameState.inProgress) {
+    return;
+  }
+  return <button onClick={onReset} className="reset-button">Reset</button>;
+};
+
+export default Reset
